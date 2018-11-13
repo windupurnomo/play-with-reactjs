f2ba980e6eac63563bd8cb3499eb28be106bdf85
Windu Purnomo <wpwindupurnomo@gmail.com>
2018-11-13 16:25:19 +0700

    cities case

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  state = {
+    appName: 'App Cities',
+    mode: 1,
+    cities: [
+      {id: 1, name: 'aceh'},
+      {id: 2, name: 'jabar'},
+      {id: 3, name: 'jateng'},
+    ],
+  };
+
+  componentDidMount() {
+    console.log('comp did mount');
+    axios.get('https://api.etanee.id/provinsi').then(r => {
+      this.setState({cities: r.data});
+    });
+  }
+  
+  x = () => this.state.cities.map(c => {
+    return <li key={c.id}>{`${c.id} - ${c.name}`}</li>;
+  });
+
+  y = () => <ul>{this.x()}</ul>;
+
+  a = () => this.state.cities.map(c => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <tr key={c.id}>
+        <td>{c.id}</td>
+        <td>{c.name}</td>
+      </tr>
+    );
+  });
+
+  b = () => (
+    <table border="1">
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <th>Nama</th>
+        </tr>
+        {this.a()}
+      </tbody>
+    </table>
+  );
+
+  handleClick = n => {
+    console.log('handle click');
+    this.setState({mode: n});
+  };
+
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <h1>{this.state.appName}</h1>
+        <button onClick={e => this.handleClick(1)}>List</button>
+        <button onClick={e => this.handleClick(2)}>Table</button>
+        {this.state.mode === 1 ? this.y() : this.b()}
       </div>
     );
   }
